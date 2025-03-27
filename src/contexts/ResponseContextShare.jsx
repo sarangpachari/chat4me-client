dc08dc3ff5666acf043a7c6e967924168b3fc9d3
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-03-27 19:13:55 +0530

    change profile picture completed

--- src/contexts/ResponseContextShare.jsx
+++ src/contexts/ResponseContextShare.jsx
@@ -1,13 +1,17 @@
 import React, { createContext, useState } from "react";
 export const deleteSingleMsgResponseContext = createContext();
+export const changeProfilePictureResponseContext = createContext()
 
 const ResponseContextShare = ({ children }) => {
     const [singleDeleteMsgResponse, setSingleDeleteMsgResponse] = useState("");
+    const [changeProfilePictureResponse, setChangeProfilePictureResponse] = useState("");
   return (
     <>
       {/* MESSAGE DELETE RESPONSE */}
       <deleteSingleMsgResponseContext.Provider value={{singleDeleteMsgResponse, setSingleDeleteMsgResponse}}>
+        <changeProfilePictureResponseContext.Provider value={{changeProfilePictureResponse, setChangeProfilePictureResponse}}>
         {children}
+        </changeProfilePictureResponseContext.Provider>
       </deleteSingleMsgResponseContext.Provider>
     </>
   );
